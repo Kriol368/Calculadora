d2009f7eff874e5955a6c3750d57cb878700316d
Kriol368 <andreubeltran2004@gmail.com>
2024-04-10 09:52:03 +0200

    Initial commit

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class Calculadora {
+    private JPanel root;
+    private JPanel Cabecera;
+    private JButton btnReset;
+    private JLabel lblDisplay;
+    private JPanel Botonera;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculadora");
+        frame.setContentPane(new Calculadora().root);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
